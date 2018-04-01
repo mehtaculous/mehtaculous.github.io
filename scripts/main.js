e6092283e0b98e9e29055787441e8764ab46acae
Sahil Mehta <samehta91@gmail.com>
2018-04-01 13:29:27 -0700

    Add medium blog posts

--- scripts/main.js
+++ scripts/main.js
@@ -2,25 +2,25 @@ $(document).ready(function() {
   // --------------------------------
   // WINDOW HEIGHT TESTING
   // --------------------------------
-  $(window).on('scroll', function() {
-    console.log("Window Height: ", $(window).height());
-    console.log("Document Height: ", $(document).height());
-    console.log("ScrollTop: ", $(window).scrollTop());
-  });
-  
-  
+//  $(window).on('scroll', function() {
+//    console.log("Window Height: ", $(window).height());
+//    console.log("Document Height: ", $(document).height());
+//    console.log("ScrollTop: ", $(window).scrollTop());
+//  });
+
+
   // --------------------------------
   // MORPHING HEADER TEXT
   // --------------------------------
   $("#hide").css('display', 'inline-block');
-  
+
   $("#js-rotating").Morphext({
     animation: "flipInX",
     separator: "|",
     speed: 2000
   });
-  
-  
+
+
   // --------------------------------
   // NAVBAR COLOR CHANGE ON SCROLL
   // --------------------------------
@@ -36,11 +36,11 @@ $(document).ready(function() {
       $('.navbar').removeClass('scroll');
     }
   };
-  
+
   checkScroll();
   $(window).scroll(checkScroll);
-  
-  
+
+
   // --------------------------------
   // NAVLINKS EASING TO SECTIONS
   // --------------------------------
@@ -56,23 +56,8 @@ $(document).ready(function() {
       }
     }
   });
-  
-  
-  // --------------------------------
-  // ANIMATE POEM
-  // --------------------------------
-//  var scrollDistance = $('#about .poem').offset().top - $(window).height() + 150;
-//  
-//  $(window).scroll(function(event) {
-//    if ($(window).scrollTop() >= scrollDistance) {
-//      $('#about .poem').css({
-//        opacity: 1,
-//        transform: 'scaleX(1) translateY(0)'
-//      });
-//    }
-//  });
 
-  
+
   // --------------------------------
   // TYPED.JS
   // --------------------------------
@@ -82,20 +67,20 @@ $(document).ready(function() {
     if ($(window).scrollTop() >= scrollTypedDistance && !animateTypedString)  {
       var options = {
         strings: ["^500I don't know who you are or what you want^300.^300.^300.^300but I can tell you that I have acquired a very particular set of skills over a short span of time.^1000 While I'm always experimenting with new languages,^300 frameworks,^300 and libraries to better my arsenal,^500 the array of icons below are my primary weapons of mass creation."],
-        typeSpeed: 20
+        typeSpeed: 30
       }
       var typed = new Typed('#skills-description', options);
       animateTypedString = true;
     }
   });
-  
-  
+
+
   // --------------------------------
   // SKILLS ICONS TOOLTIP
   // --------------------------------
   $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   });
-  
+
 
 });
